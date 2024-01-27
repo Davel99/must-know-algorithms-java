720e5260e069614435df7439aee9ae63175b9aa9
davel99 <joelmp3@live.com>
2024-01-27 14:50:02 -0600

    Added binary search class

---
+++ src/com/algorithm/searching/BinarySearch.java
@@ -0,0 +1,18 @@
+package com.algorithm.searching;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int[] arr = { -4, 5, 10, 12, 13, 50, 111, 120};
+
+	}
+	
+	public static int binarySearch(int[] arr, int x) {
+		int response = -1;
+		
+		
+		
+		return response;
+	}
+
+}
