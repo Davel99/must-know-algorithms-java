73c23620bdffc6c3f93acc3c19016e9bb819b0f2
davel99 <joelmp3@live.com>
2024-01-27 15:00:52 -0600

    Completed binary search algorithm

--- src/com/algorithm/searching/BinarySearch.java
+++ src/com/algorithm/searching/BinarySearch.java
@@ -1,18 +1,33 @@
 package com.algorithm.searching;
 
+import com.algorithm.utility.Utility;
+
 public class BinarySearch {
 
 	public static void main(String[] args) {
 		int[] arr = { -4, 5, 10, 12, 13, 50, 111, 120};
-
+		int search = -4;
+		int response = BinarySearch.binarySearch(arr, search);
+		String message = "";
+		if(response == -1) message = search + " not found";
+		else message = search + " was found in index " + response;
+		System.out.println(message);
+		
+		Utility.printArray(arr);
 	}
 	
 	public static int binarySearch(int[] arr, int x) {
-		int response = -1;
+		int low = 0;
+		int high = arr.length -1;
+		int mid;
 		
-		
-		
-		return response;
+		while (low <= high) {
+			mid = low + (high - low)/2;
+			if(arr[mid] == x) return mid;
+			if(arr[mid] < x) low = mid + 1;
+			else high = mid -1;
+		}
+		return -1;
 	}
 
 }
