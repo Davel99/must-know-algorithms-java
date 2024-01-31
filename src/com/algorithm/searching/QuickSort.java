d732a97610e6206482b5be30135ab6fdf5b00a94
davel99 <joelmp3@live.com>
2024-01-30 21:57:49 -0600

    Started Quick Sort algorithm

---
+++ src/com/algorithm/searching/QuickSort.java
@@ -0,0 +1,14 @@
+package com.algorithm.searching;
+
+import com.algorithm.utility.Utility;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] arr = { 6, -3, 105, -46, 57 };
+		
+		
+		Utility.printArray(arr);
+	}
+
+}
